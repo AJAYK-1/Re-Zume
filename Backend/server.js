900a641e0e0b61e9f52bf3c23c8d5074aaa11c94
AJAYK-1 <ajaykumar1shy@gmail.com>
2025-09-28 22:24:10 +0530

    "few changes"

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-dotenv.config()
+import express, { json } from 'express'
+import { config } from 'dotenv'
+import userRouter from './Routers/userRouter.js'
+import helmet from 'helmet'
 
-app.use(express.json())
+const app = express()
+config()
+
+app.use(json())
+app.use(helmet())
+
+app.use('/api/user', userRouter)
 
 app.listen(process.env.PORT || 5000,
     () => console.log('Server Running at PORT: ', process.env.PORT))
