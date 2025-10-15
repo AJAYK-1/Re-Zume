12cf6339b5c4bfa8f01c999854a60251a554c210
AJAYK-1 <ajaykumar1shy@gmail.com>
2025-10-15 22:04:06 +0530

    "Resume generator"

--- Backend/Utilities/ResumeGenerator.js
+++ Backend/Utilities/ResumeGenerator.js
@@ -18,11 +18,37 @@ const ResumeGenerator = async (resumeData) => {
         const fileName = `${name.split(' ').join('')}_${Date.now()}.pdf`
         const filePath = path.join(dir, fileName)
 
-        const doc = new PDFDocument()
+        const doc = new PDFDocument({ size: 'A4', margins: { left: 36, right: 36, top: 36, bottom: 36 } })
         const stream = fs.createWriteStream(filePath)
         doc.pipe(stream)
 
-        doc.fontSize(25).text(name, { align: 'center' })
+        doc.font('Times-Roman').fontSize(28).text(`${name}`, { align: 'center', })
+        doc.moveDown(0.5)
+
+        doc.font('Helvetica').fontSize(10).text(`${address.city}, ${address.district}, ${address.pincode}`, 140, 80, { continued: true })
+        doc.text(' | ', { continued: true })
+        doc.text(`${phone}`, { link: `tel:${phone}`, continued: true })
+        doc.text(' | ', { continued: true })
+        doc.fillColor('blue').text(`${email}`, { link: `mailto:${email}`, underline: true })
+        doc.moveDown(0.6)
+
+        if (linkedIn) {
+            doc.fillColor('black').text('[ ', 150, 10, { fillColor: 'black', continued: true })
+            doc.fillColor('blue').text('LinkedIn', { link: `${linkedIn}`, continued: true, underline: true })
+            doc.fillColor('black').text(' ]', { fillColor: 'black', continued: true })
+        }
+
+        if (gitHub) {
+            doc.fillColor('black').text('[ ', { fillColor: 'black', continued: true })
+            doc.fillColor('blue').text('GitHub', { link: `${gitHub}`, continued: true, underline: true })
+            doc.fillColor('black').text(' ]', { fillColor: 'black', continued: true })
+        }
+
+        if (portfolio) {
+            doc.fillColor('black').text('[ ', { fillColor: 'black', continued: true })
+            doc.fillColor('blue').text('Portfolio', { link: `${portfolio}`, continued: true, underline: true })
+            doc.fillColor('black').text(' ]', { fillColor: 'black', continued: true })
+        }
 
         doc.end()
         await new Promise((resolve, reject) => {
