68c23e0826ce53bc4d5bb2fbfbcdd62df7614cf2
AJAYK-1 <ajaykumartp10@gmail.com>
2025-11-16 22:00:00 +0530

    "Education section"

--- Frontend/src/Pages/User/components/Education.jsx
+++ Frontend/src/Pages/User/components/Education.jsx
@@ -4,7 +4,14 @@ function Education() {
   return (
     <div className='py-3'>
       <h2 className='sub-heading'> Education </h2>
-      
+      <div>
+        <form action="">
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </form>
+      </div>
     </div>
   )
 }
