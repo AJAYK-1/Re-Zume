a487e269b5c48246df47900f8f7798fcaba6b0cb
AJAYK-1 <ajaykumartp10@gmail.com>
2025-11-19 20:58:15 +0530

    "cert/workshop"

--- Frontend/src/Pages/User/components/Certification.jsx
+++ Frontend/src/Pages/User/components/Certification.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 function Certification() {
   return (
     <div className='py-3'>
-      <h2 className='sub-heading'> Certifications and Workshops </h2>
-      <input type="text" />
-      <input type="text" />
+      <h2 className='sub-heading' > Certifications and Workshops </h2>
+      <input type="text"
+        placeholder='Certificate/Workshop name...'
+        className='resume-input' />
+      <input type="text"
+        placeholder='Certificate Provider...'
+        className='resume-input' />
     </div>
   )
 }
