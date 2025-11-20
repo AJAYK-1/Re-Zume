2e6a788e4f54c69487d257e565b6bd7fa8ec6b1f
AJAYK-1 <ajaykumartp10@gmail.com>
2025-11-20 20:56:17 +0530

    "education section"

--- Frontend/src/Pages/User/components/Education.jsx
+++ Frontend/src/Pages/User/components/Education.jsx
@@ -7,14 +7,21 @@ function Education() {
       <h2 className='sub-heading'> Education </h2>
       <div>
         <form action="">
-          <input type="text" 
-          className='resume-input'/>
-          <input type="text" 
-          className='resume-input'/>
-          <input type="text" 
-          className='resume-input'/>
-          <input type="text" 
-          className='resume-input'/>
+          <input type="text"
+            className='resume-input'
+            placeholder='institution' />
+          <input type="text"
+            className='resume-input'
+            placeholder='university' />
+          <input type="text"
+            className='resume-input'
+            placeholder='course' />
+          <input type="month"
+            className='resume-input'
+            placeholder='from' />
+          <input type="month"
+            className='resume-input'
+            placeholder='to' />
           <button type='submit' className='button-3 absolute right-2 top-10' > <FaChevronRight /> </button>
         </form>
       </div>
