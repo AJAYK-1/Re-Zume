a189ef5c53e905374fa617e9b6c0bdad721eab84
AJAYK-1 <ajaykumartp10@gmail.com>
2025-11-23 21:33:59 +0530

    "Project section"

--- Frontend/src/Pages/User/components/Projects.jsx
+++ Frontend/src/Pages/User/components/Projects.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useResumeData } from '../../../Context/resumeContext'
 
 const Projects = () => {
     const { resumeData, setResumeData, step, setStep } = useResumeData()
-
+    const [project, setProject] = useState([])
+    
     return (
         <div className='py-3'>
             <h2 className='sub-heading'> Projects </h2>
-            
 
+            <form>
+                <input type="text" placeholder='Title...' />
+                <input type="text" placeholder='Description...' />
+                <input type="text" placeholder='Link' />
+            </form>
         </div>
     )
 }
